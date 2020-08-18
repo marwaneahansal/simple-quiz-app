e3c274a4952f12aa49fe32d58eb50cb4e34a2ad4
marwane ahansal <ahansalmarwane@gmail.com>
2020-08-18 16:09:16 +0100

    added circle to show result of score

---
+++ quiz_app/lib/helperWidget/CircleScoe.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CircleProgress extends CustomPainter {
+  double correctAnswers;
+
+  CircleProgress({this.correctAnswers});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint outerCircle = Paint()
+      ..strokeWidth = 7
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke;
+
+    Paint completArc = Paint()
+      ..strokeWidth = 7
+      ..style = PaintingStyle.stroke
+      ..color = Colors.redAccent
+      ..strokeCap = StrokeCap.round;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+
+    double raduis = min(size.width / 2, size.height / 2) - 7;
+
+    canvas.drawCircle(center, raduis, outerCircle);
+
+    double angle = 2 * pi * ((correctAnswers * 10) / 100);
+    canvas.drawArc(Rect.fromCircle(center: center, radius: raduis), -pi / 2,
+        angle, false, completArc);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- quiz_app/lib/score.dart
+++ quiz_app/lib/score.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'helperWidget/CircleScoe.dart';
+
 class Score extends StatefulWidget {
   final List<String> questions;
   final List<String> correctAnswer;
@@ -10,27 +12,77 @@ class Score extends StatefulWidget {
   _ScoreState createState() => _ScoreState();
 }
 
-class _ScoreState extends State<Score> {
+class _ScoreState extends State<Score> with SingleTickerProviderStateMixin {
+  AnimationController scoreAnimationControler;
+  Animation animation;
+  @override
+  void initState() {
+    List<String> userAnswersKeys = widget.userAnswers.keys.toList();
+    super.initState();
+
+    scoreAnimationControler = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+    animation = Tween(begin: 0, end: 9.0).animate(scoreAnimationControler)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
+    scoreAnimationControler.forward();
     return Scaffold(
-      body: SafeArea(
-        child: Expanded(
-          child: ListView.builder(
-            itemCount: widget.userAnswers.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  Text('${widget.userAnswers.keys.toList()[index]}: '),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text('${widget.userAnswers.values.toList()[index]}: '),
-                ],
-              );
-            },
-          ),
+      backgroundColor: Color(0XFFF3F3F4),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0XFFF3F3F4),
+        title: Text(
+          // 'General Knowledge',
+          'Your Score',
+          style: TextStyle(color: Colors.black87),
         ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            color: Colors.black87,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          CustomPaint(
+            foregroundPainter:
+                CircleProgress(correctAnswers: animation.value.toDouble()),
+            child: Container(
+              width: 200,
+              height: 200,
+              child: Center(child: Text('${animation.value.toInt()}/10')),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.userAnswers.length,
+              itemBuilder: (context, index) {
+                return Wrap(
+                  children: [
+                    Text('${widget.questions[index]}: '),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('${widget.userAnswers.values.toList()[index]} '),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
